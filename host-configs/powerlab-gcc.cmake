b5f99bc48d42464cefe18f57a54b3a9c7dc86d94
Stephanie Labasan <labasan1@llnl.gov>
2018-08-30 14:40:41 -0700

    Refactoring HWLOC dependency:     - First, check for user-specifed path to intall     - Second, locate files on the system     - Third, download and build from source as ExternalProject
    Remove install step of ExternalProject. If CMAKE_INSTALL_PREFIX is default
    value of /usr/local, the install will fail due to incorrect permissions.
    
    Signed-off-by: Stephanie Labasan <labasan1@llnl.gov>

---
+++ host-configs/powerlab-gcc.cmake
@@ -0,0 +1,12 @@
+# c compiler
+set(CMAKE_C_COMPILER "gcc" CACHE PATH "")
+
+# cpp compiler
+set(CMAKE_CXX_COMPILER "g++" CACHE PATH "")
+
+# fortran compiler
+set(CMAKE_Fortran_COMPILER  "gfortran" CACHE PATH "")
+
+set(BUILD_DOCS OFF CACHE PATH "")
+set(BUILD_TESTING ON CACHE PATH "")
+set(ENABLE_TESTS ON CACHE PATH "")
